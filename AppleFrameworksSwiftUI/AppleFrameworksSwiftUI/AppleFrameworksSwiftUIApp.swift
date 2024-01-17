0bdcf84734106b68c9bd37f4313fbaddd5545ed8
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-01-17 19:36:09 +0300

    0.3

--- AppleFrameworksSwiftUI/AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
+++ AppleFrameworksSwiftUI/AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
@@ -10,8 +10,11 @@ import SwiftUI
 @main
 struct AppleFrameworksSwiftUIApp: App {
     var body: some Scene {
+        // Ana pencere grubunu belirliyoruz.
         WindowGroup {
+            // FrameworkGridView yapısını ana içerik olarak kullanıyoruz.
             FrameworkGridView()
         }
     }
 }
+
